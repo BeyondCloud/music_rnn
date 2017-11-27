80579f177f6bafe1b59dbe09ee4ead179bc662c7
BeyondCloud <a1989796@yahoo.com.tw>
2017-11-27 18:15:07 +0800

    simplify code

--- rnn_restore.py
+++ rnn_restore.py
@@ -15,37 +15,7 @@ import nottingham_util
 import util
 from model import Model, NottinghamModel
 from rnn import DefaultConfig
-# def get_config_name(config):
-#     def replace_dot(s): return s.replace(".", "p")
-#     return "nl_" + str(config.num_layers) + "_hs_" + str(config.hidden_size) + \
-#             replace_dot("_mc_{}".format(config.melody_coeff)) + \
-#             replace_dot("_dp_{}".format(config.dropout_prob)) + \
-#             replace_dot("_idp_{}".format(config.input_dropout_prob)) + \
-#             replace_dot("_tb_{}".format(config.time_batch_len)) 
 
-# class DefaultConfig(object):
-#     # model parameters
-#     num_layers = 2
-#     hidden_size = 200
-#     melody_coeff = 0.5
-#     dropout_prob = 0.5
-#     input_dropout_prob = 0.8
-#     cell_type = 'lstm'
-
-#     # learning parameters
-#     max_time_batches = 9 
-#     time_batch_len = 128
-#     learning_rate = 5e-3
-#     learning_rate_decay = 0.9
-#     num_epochs = 250
-
-#     # metadata
-#     dataset = 'softmax'
-#     model_file = ''
-
-#     def __repr__(self):
-#         return """Num Layers: {}, Hidden Size: {}, Melody Coeff: {}, Dropout Prob: {}, Input Dropout Prob: {}, Cell Type: {}, Time Batch Len: {}, Learning Rate: {}, Decay: {}""".format(self.num_layers, self.hidden_size, self.melody_coeff, self.dropout_prob, self.input_dropout_prob, self.cell_type, self.time_batch_len, self.learning_rate, self.learning_rate_decay)
-    
 if __name__ == '__main__':
     np.random.seed()      
 
@@ -63,7 +33,6 @@ if __name__ == '__main__':
 
 
     if args.dataset == 'softmax':
-        resolution = 480
         time_step = 120
         model_class = NottinghamModel
         with open(nottingham_util.PICKLE_LOC, 'r') as f:
@@ -89,103 +58,79 @@ if __name__ == '__main__':
     logger.addHandler(logging.StreamHandler())
     logger.addHandler(logging.FileHandler(os.path.join(run_folder, "training.log")))
 
-    grid = {
-        "dropout_prob": [0.5],
-        "input_dropout_prob": [0.8],
-        "melody_coeff": [0.5],
-        "num_layers": [2],
-        "hidden_size": [200],
-        "num_epochs": [250],
-        "learning_rate": [5e-3],
-        "learning_rate_decay": [0.9],
-        "time_batch_len": [128],
-    }
-
-    # Generate product of hyperparams
-    runs = list(list(itertools.izip(grid, x)) for x in itertools.product(*grid.itervalues()))
-    logger.info("{} runs detected".format(len(runs)))
     with open(args.config_file, 'r') as f: 
         config = cPickle.load(f)
+    print(config) #print config and add to log
 
 
-    for combination in runs:
-        # nl_2_hs_200_mc_0p5_dp_0p5_idp_0p8_tb_128.config
-    
+    if config.dataset == 'softmax':
+        data = util.load_data('', time_step, config.time_batch_len, config.max_time_batches, nottingham=pickle)
+    else:
+        raise Exception("Other datasets not yet implemented")
 
-        for attr, value in combination:
-            setattr(config, attr, value)
 
-        if config.dataset == 'softmax':
-            data = util.load_data('', time_step, config.time_batch_len, config.max_time_batches, nottingham=pickle)
-        else:
-            raise Exception("Other datasets not yet implemented")
 
-        logger.info(config) #print config and add to log
+    with tf.Graph().as_default(), tf.Session() as session:
+        with tf.variable_scope("model", reuse=None):
+            train_model = model_class(config, training=True)
+        with tf.variable_scope("model", reuse=True):
+            valid_model = model_class(config, training=False)
 
-        with open(args.config_file, 'w') as f: 
-            cPickle.dump(config, f)
+        saver = tf.train.Saver(tf.all_variables(), max_to_keep=40)
+        saver.restore(session, os.path.join(run_folder, config.model_name))
+        # training
+        early_stop_best_loss = None
+        start_saving = False
+        saved_flag = False
+        train_losses, valid_losses = [], []
+        start_time = time.time()
+        for i in range(config.num_epochs):
+            loss = util.run_epoch(session, train_model, 
+                data["train"]["data"], training=True, testing=False)
+            train_losses.append((i, loss))
+            if i == 0:
+                continue
 
-        with tf.Graph().as_default(), tf.Session() as session:
-            with tf.variable_scope("model", reuse=None):
-                train_model = model_class(config, training=True)
-            with tf.variable_scope("model", reuse=True):
-                valid_model = model_class(config, training=False)
+            logger.info('Epoch: {}, Train Loss: {}, Time Per Epoch: {}'.format(\
+                    i, loss, (time.time() - start_time)/i))
+            valid_loss = util.run_epoch(session, valid_model, data["valid"]["data"], training=False, testing=False)
+            valid_losses.append((i, valid_loss))
+            logger.info('Valid Loss: {}'.format(valid_loss))
 
-            saver = tf.train.Saver(tf.all_variables(), max_to_keep=40)
-            saver.restore(session, os.path.join(run_folder, config.model_name))
-            # training
-            early_stop_best_loss = None
-            start_saving = False
-            saved_flag = False
-            train_losses, valid_losses = [], []
-            start_time = time.time()
-            for i in range(config.num_epochs):
-                loss = util.run_epoch(session, train_model, 
-                    data["train"]["data"], training=True, testing=False)
-                train_losses.append((i, loss))
-                if i == 0:
-                    continue
-
-                logger.info('Epoch: {}, Train Loss: {}, Time Per Epoch: {}'.format(\
-                        i, loss, (time.time() - start_time)/i))
-                valid_loss = util.run_epoch(session, valid_model, data["valid"]["data"], training=False, testing=False)
-                valid_losses.append((i, valid_loss))
-                logger.info('Valid Loss: {}'.format(valid_loss))
-
-                if early_stop_best_loss == None:
-                    early_stop_best_loss = valid_loss
-                elif valid_loss < early_stop_best_loss:
-                    early_stop_best_loss = valid_loss
-                    if start_saving:
-                        logger.info('Best loss so far encountered, saving model.')
-                        saver.save(session, os.path.join(run_folder, config.model_name))
-                        saved_flag = True
-                elif not start_saving:
-                    start_saving = True 
-                    logger.info('Valid loss increased for the first time, will start saving models')
+            if early_stop_best_loss == None:
+                early_stop_best_loss = valid_loss
+            elif valid_loss < early_stop_best_loss:
+                early_stop_best_loss = valid_loss
+                if start_saving:
+                    logger.info('Best loss so far encountered, saving model.')
                     saver.save(session, os.path.join(run_folder, config.model_name))
                     saved_flag = True
-
-            if not saved_flag:
+            elif not start_saving:
+                start_saving = True 
+                logger.info('Valid loss increased for the first time, will start saving models')
                 saver.save(session, os.path.join(run_folder, config.model_name))
+                saved_flag = True
 
-            # set loss axis max to 20
-            axes = plt.gca()
-            if config.dataset == 'softmax':
-                axes.set_ylim([0, 2])
-            else:
-                axes.set_ylim([0, 100])
-            plt.plot([t[0] for t in train_losses], [t[1] for t in train_losses])
-            plt.plot([t[0] for t in valid_losses], [t[1] for t in valid_losses])
-            plt.legend(['Train Loss', 'Validation Loss'])
-            chart_file_path = os.path.join(run_folder, 'result.png')
-            plt.savefig(chart_file_path)
-            plt.clf()
+        if not saved_flag:
+            saver.save(session, os.path.join(run_folder, config.model_name))
 
-            logger.info("Config {}, Loss: {}".format(config, early_stop_best_loss))
-            if best_valid_loss == None or early_stop_best_loss < best_valid_loss:
-                logger.info("Found best new model!")
-                best_valid_loss = early_stop_best_loss
-                best_config = config
+        # set loss axis max to 20
+        axes = plt.gca()
+        if config.dataset == 'softmax':
+            axes.set_ylim([0, 2])
+        else:
+            axes.set_ylim([0, 100])
+        plt.plot([t[0] for t in train_losses], [t[1] for t in train_losses])
+        plt.plot([t[0] for t in valid_losses], [t[1] for t in valid_losses])
+        plt.legend(['Train Loss', 'Validation Loss'])
+        chart_file_path = os.path.join(run_folder, 'result.png')
+        plt.savefig(chart_file_path)
+        plt.clf()
+
+        logger.info("Config {}, Loss: {}".format(config, early_stop_best_loss))
+        if best_valid_loss == None or early_stop_best_loss < best_valid_loss:
+            logger.info("Found best new model!")
+            best_valid_loss = early_stop_best_loss
+            best_config = config
 
     logger.info("Best Config: {}, Loss: {}".format(best_config, best_valid_loss))
